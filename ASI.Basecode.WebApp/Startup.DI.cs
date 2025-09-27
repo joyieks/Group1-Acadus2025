75baf1c60649973356d9f297a42d63c2595fd07e
hanphil <zeryislet@gmail.com>
2025-09-27 15:11:39 +0800

    added Create and Read

--- ASI.Basecode.WebApp/Startup.DI.cs
+++ ASI.Basecode.WebApp/Startup.DI.cs
@@ -35,10 +35,11 @@ namespace ASI.Basecode.WebApp
             // Services
             this._services.TryAddSingleton<TokenValidationParametersFactory>();
             this._services.AddScoped<IUserService, UserService>();
-          
+            this._services.AddScoped<IBookService, BookService>();
 
             // Repositories
             this._services.AddScoped<IUserRepository, UserRepository>();
+            this._services.AddScoped<IBookRepository, BookRepository>();
 
             // Manager Class
             this._services.AddScoped<SignInManager>();
