b0275ca0dd5eedf810b7867e069ed9d03895a762
Joshua Jakosalem <joshua.jakosalem.m@gmail.com>
2025-10-11 08:13:15 +0800

    feat(layout): Created Courses layout and respective course card component for teacher modules. Also refactored Recent Activity component to use DataTable library for easy pagination.

--- ASI.Basecode.WebApp/Controllers/TeacherController.cs
+++ ASI.Basecode.WebApp/Controllers/TeacherController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ASI.Basecode.WebApp.Models;
+using System.Collections.Generic;
 
 namespace ASI.Basecode.WebApp.Controllers
 {
@@ -18,15 +19,55 @@ namespace ASI.Basecode.WebApp.Controllers
         }
 
         [HttpGet]
-        public IActionResult Activities()
+        public IActionResult Courses()
         {
-            return View();
-        }
+            var courses = new List<TeacherCourseViewModel>
+            {
+                new TeacherCourseViewModel
+                {
+                    CourseCode = "91299 - ELPHP41",
+                    CourseTitle = "FREE ELECTIVE - PHP",
+                    SemesterInfo = "1st Semester 2025 - 2026",
+                    CardColor = "#E8F9E8"
+                },
+                new TeacherCourseViewModel
+                {
+                    CourseCode = "91300 - CS101",
+                    CourseTitle = "INTRODUCTION TO COMPUTER SCIENCE",
+                    SemesterInfo = "1st Semester 2025 - 2026",
+                    CardColor = "#D1FAE5"
+                },
+                new TeacherCourseViewModel
+                {
+                    CourseCode = "91301 - MATH201",
+                    CourseTitle = "DISCRETE MATHEMATICS",
+                    SemesterInfo = "1st Semester 2025 - 2026",
+                    CardColor = "#A7F3D0"
+                },
+                new TeacherCourseViewModel
+                {
+                    CourseCode = "91302 - ENG102",
+                    CourseTitle = "TECHNICAL WRITING",
+                    SemesterInfo = "1st Semester 2025 - 2026",
+                    CardColor = "#6EE7B7"
+                },
+                new TeacherCourseViewModel
+                {
+                    CourseCode = "91303 - DATA301",
+                    CourseTitle = "DATA STRUCTURES",
+                    SemesterInfo = "2nd Semester 2025 - 2026",
+                    CardColor = "#34D399"
+                },
+                new TeacherCourseViewModel
+                {
+                    CourseCode = "91304 - WEBDEV401",
+                    CourseTitle = "WEB DEVELOPMENT",
+                    SemesterInfo = "2nd Semester 2025 - 2026",
+                    CardColor = "#10B981"
+                }
+            };
 
-        [HttpGet]
-        public IActionResult Grades()
-        {
-            return View();
+            return View(courses.ToArray());
         }
     }
 }
